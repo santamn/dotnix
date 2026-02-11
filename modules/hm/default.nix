963f0be0fd9f0eba5e2813f4c6bd37a93a7a500f
santamn <cle.neige@gmail.com>
2026-02-11 22:35:49 +0900

    感度3000倍

--- modules/hm/default.nix
+++ modules/hm/default.nix
@@ -100,8 +100,8 @@
           # 以前の設定を無効化
           gesture = 3, horizontal, unset
           # 3本指の水平スワイプでワークスペースを切り替え
-          # 指の動きに対して2倍の速度でアニメーションが進行するように設定
-          gesture = 3, horizontal, workspace, scale: 20.0
+          # 指の動きに対して3000倍の速度でアニメーションが進行するように設定
+          gesture = 3, horizontal, workspace, scale: 3000.0
         }
       '';
 
