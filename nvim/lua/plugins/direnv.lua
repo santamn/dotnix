e6112a4ced56e06fd5d7bbfd4ef1cf2b1a39e0cd
santamn <cle.neige@gmail.com>
2026-02-17 11:58:59 +0900

    direnvの遅延読み込みを無効化

--- nvim/lua/plugins/direnv.lua
+++ nvim/lua/plugins/direnv.lua
@@ -1,5 +1,6 @@
 ---@type LazySpec
 return {
   "direnv/direnv.vim",
-  event = "VeryLazy", -- または "User AstroFile"
+  lazy = false,    -- 遅延読み込みを無効化し、起動時に即座に読み込む
+  priority = 1000, -- 他のプラグイン（LSPなど）より先に読み込む
 }
