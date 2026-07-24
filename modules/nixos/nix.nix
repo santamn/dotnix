790c3600b888d55174e8b8570f1ccf67145d3b18
santamn <cle.neige@gmail.com>
2026-07-24 17:47:25 +0900

    gcにnhを使用

--- modules/nixos/nix.nix
+++ modules/nixos/nix.nix
@@ -16,12 +16,17 @@
       connect-timeout = 10;
       stalled-download-timeout = 60;
     };
+  };
 
-    # 古い世代を毎週自動削除
-    gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 30d";
+  # nh (nix helper): rebuild や GC を使いやすくするラッパー CLI
+  programs.nh = {
+    enable = true;
+    # 古い世代 (30日超) を毎日自動削除
+    # nix.gc と違ってユーザープロファイルや gcroots になっている result シンボリックリンクもまとめて削除する
+    clean = {
+      enable = true;
+      dates = "daily";
+      extraArgs = "--keep-since 30d";
     };
   };
 }
